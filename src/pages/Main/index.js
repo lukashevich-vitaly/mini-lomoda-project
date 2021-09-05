c5794046d36d64daa7a4dc2b3c4e5e4252161b7f
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2021-09-05 16:14:11 +0300

    fix: fixed footer

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -8,37 +8,21 @@ import child from '../../image/child.jpg';
 
 const Main = () => {
     return (
-        <div>
-           <section className="promo">
-            <div className="container">
-                <ul className="promo__grid">
-                    <li className="promo__grid-item promo__grid-item__big"><img className="promo__grid-image" src={beach} alt="Девушка на пляже" /></li>
-                    <li className="promo__grid-item promo__grid-item__small"><img className="promo__grid-image" src={sport} alt="Девушка на спорте" /></li>
-                    <li className="promo__grid-item promo__grid-item__small"><img className="promo__grid-image" src={umbrella} alt="Девушка с зонтом" /></li>
-                </ul>
+      <section className="promo">
+        <div className="container">
+            <ul className="promo__grid">
+                <li className="promo__grid-item promo__grid-item__big"><img className="promo__grid-image" src={beach} alt="Девушка на пляже" /></li>
+                <li className="promo__grid-item promo__grid-item__small"><img className="promo__grid-image" src={sport} alt="Девушка на спорте" /></li>
+                <li className="promo__grid-item promo__grid-item__small"><img className="promo__grid-image" src={umbrella} alt="Девушка с зонтом" /></li>
+            </ul>
 
-                <ul className="promo__grid promo__grid-reverse">
-                    <li className="promo__grid-reverse-item promo__grid-reverse-item__big"><img className="promo__grid-image" src={premium} alt="Девушка в деловом стиле" /></li>
-                    <li className="promo__grid-reverse-item promo__grid-reverse-item__small"><img className="promo__grid-image" src={sneakers} alt="Кроссовки" /></li>
-                    <li className="promo__grid-reverse-item promo__grid-reverse-item__small"><img className="promo__grid-image" src={child} alt="Два ребенка" /></li>
-                </ul>
-            </div>
-        </section>
-
-        <div className='main'>
-            <h1>
-                MAIN
-            </h1>
-            <p className='text'>
-                Одной из причин быстрого роста популярности flexbox среди веб-разработчиков было то, 
-                что впервые были предоставлены адекватные возможности выравнивания. Он предоставил 
-                адекватное вертикальное выравнивание, и стало возможным, наконец, легко поместить 
-                элемент в центр по вертикали. В этом руководстве детально рассматривается, как 
-                выравнивание и распределение работают во Flexbox.
-            </p>
-        </div> 
-        </div>
-        
+            <ul className="promo__grid promo__grid-reverse">
+                <li className="promo__grid-reverse-item promo__grid-reverse-item__big"><img className="promo__grid-image" src={premium} alt="Девушка в деловом стиле" /></li>
+                <li className="promo__grid-reverse-item promo__grid-reverse-item__small"><img className="promo__grid-image" src={sneakers} alt="Кроссовки" /></li>
+                <li className="promo__grid-reverse-item promo__grid-reverse-item__small"><img className="promo__grid-image" src={child} alt="Два ребенка" /></li>
+            </ul>
+         </div>
+      </section>
     )
 };
 
